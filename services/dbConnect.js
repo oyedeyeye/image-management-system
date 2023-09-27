42dcfbed64648a34a1390f3dca9ec63200b46749
Hezekiah Oyemade <oyedey@gmail.com>
2023-09-27 12:09:36 +0100

    refactored the db connection for production

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -22,15 +22,27 @@ require('dotenv').config();
 
 // connectDB();
 
+// Set the production variable. This will be called when deployed to a live host
+const isProduction = process.env.NODE_ENV === 'production';
+
+// configuration details
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+// if the project has been deployed, connect with the host's DATABASE_URL
+// else connect with the local DATABASE_URL
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+  /*
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000,
-});
+  idleTimeoutMillis: 30000,*/
+}); 
 
 pool.on('connect', () => console.log("Successfully connected to the Database"));
 
