fefddd19596a9fec59419dd3c8641df8badc2385
Hezekiah Oyemade <oyedey@gmail.com>
2023-10-03 22:27:40 +0100

    newDB

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -1,28 +1,78 @@
 const Pool = require("pg").Pool;
 require('dotenv').config();
 
-// const connectDB = async () => {
-//   try {
-//     const pool = new Pool({
-//       user: process.env.PGUSER,
-//       host: process.env.PGHOST,
-//       database: process.env.PGDATABASE,
-//       password: process.env.PGPASSWORD,
-//       port: process.env.PGPORT,
-//       max: 10, // max number of clients in the pool
-//       idleTimeoutMillis: 30000,
-//     });
 
-//     await pool.connect('success', () => console.log("Successfully connected to the Database"));
+/*
+const connectDB = () => {
+  try {
+    const pool = new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000,
+    });
+// 
+    pool.connect('success', () => console.log("Successfully connected to the Database"));
 
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+    const createTables = () => {
+      const dbTables = `
+      -- const usersTable
+      CREATE TABLE IF NOT EXISTS
+        users(
+          user_id INT GENERATED ALWAYS AS IDENTITY,
+          first_name VARCHAR(128) NOT NULL,
+          last_name VARCHAR(128) NOT NULL,
+          email VARCHAR(128) NOT NULL,
+          password VARCHAR(128) NOT NULL,
+          role VARCHAR(128) NOT NULL,
+          created_at TIMESTAMP DEFAULT NOW(),
+          PRIMARY KEY(user_id)
+        );
+    
+      -- const imageTable
+      CREATE TABLE IF NOT EXISTS
+        images_tabl(
+          id INT GENERATED ALWAYS AS IDENTITY,
+          title VARCHAR(128) NOT NULL,
+          cloudinary_id VARCHAR(128) NOT NULL,
+          image_url VARCHAR(128) NOT NULL,
+          creator INT,
+          created_at TIMESTAMP DEFAULT NOW(),
+          PRIMARY KEY(id),
+          CONSTRAINT fk_user
+            FOREIGN KEY(creator)
+              REFERENCES users(user_id)
+              ON DELETE SET NULL
+        )`;
+    
+      // connectDB. 
+      pool
+      .query(dbTables)
+        .then((result) => {
+          console.log(result);
+          pool.end();
+        })
+        .catch((error) => {
+          console.error(error);
+          pool.end();
+        });
+    };
 
-// connectDB();
+    return {
+      createTables: createTables
+    };
+  } catch (error) {
+    console.error(error);
+  };
+};
+connectDB();
 
-// Set the production variable. This will be called when deployed to a live host
+*/
+
+/* Set the production variable. This will be called when deployed to a live host
 const isProduction = process.env.NODE_ENV === 'production';
 
 // configuration details
@@ -30,35 +80,57 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 // if the project has been deployed, connect with the host's DATABASE_URL
 // else connect with the local DATABASE_URL
+*/
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
-  /*
+  // connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  // ssl: isProduction,
+
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000,*/
+  idleTimeoutMillis: 30000,
 }); 
 
 pool.on('connect', () => console.log("Successfully connected to the Database"));
 
 const createTables = () => {
-  const imageTable = `CREATE TABLE IF NOT EXISTS
-    images_tabl(
-      id SERIAL PRIMARY KEY,
-      title VARCHAR(128) NOT NULL,
-      cloudinary_id VARCHAR(128) NOT NULL,
-      image_url VARCHAR(128) NOT NULL,
-      created_at TIMESTAMP DEFAULT NOW()
+  const dbTables = `
+  -- const usersTable
+  CREATE TABLE IF NOT EXISTS
+    users(
+      user_id INT GENERATED ALWAYS AS IDENTITY,
+      first_name VARCHAR(128) NOT NULL,
+      last_name VARCHAR(128) NOT NULL,
+      email VARCHAR(128) NOT NULL,
+      password VARCHAR(128) NOT NULL,
+      role VARCHAR(128) NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      PRIMARY KEY(user_id)
     )`;
 
-  /* connectDB. */
+  // -- const imageTable
+  // CREATE TABLE IF NOT EXISTS
+    // images_tabl(
+      // id INT GENERATED ALWAYS AS IDENTITY,
+      // title VARCHAR(128) NOT NULL,
+      // cloudinary_id VARCHAR(128) NOT NULL,
+      // image_url VARCHAR(128) NOT NULL,
+      // creator INT,
+      // created_at TIMESTAMP DEFAULT NOW(),
+      // PRIMARY KEY(id),
+      // CONSTRAINT fk_user
+        // FOREIGN KEY(creator)
+          // REFERENCES users(user_id)
+          // ON DELETE SET NULL
+    // )`;
+
+  //  connectDB. 
   pool
-  .query(imageTable)
+  .query(dbTables)
     .then((result) => {
       console.log(result);
       pool.end();
